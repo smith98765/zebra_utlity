274a5347e5cf1eeed94fcb80a4bc51f1c6ed5dc8
Smith KEYFields <146061997+smith98765@users.noreply.github.com>
2026-07-02 18:57:22 +0630

    Update Printer.java

--- android/src/main/java/com/professor/zebrautility/Printer.java
+++ android/src/main/java/com/professor/zebrautility/Printer.java
@@ -607,8 +607,7 @@ public class Printer implements MethodChannel.MethodCallHandler {
         } else if (call.method.equals("checkPermission")) {
             checkPermission(context, result);
         } else if (call.method.equals("convertBase64ImageToZPLString")) {
-            convertBase64ImageToZPLString(call.argument("Data").toString())
-                    , Integer.valueOf(call.argument("rotation").toString()), result);
+            convertBase64ImageToZPLString(call.argument("Data").toString(), Integer.valueOf(call.argument("rotation").toString()), result);
         } else if (call.method.equals("disconnect")) {
             new Thread(new Runnable() {
                 @Override
